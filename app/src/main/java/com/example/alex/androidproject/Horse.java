528ec137addb4715b4625784520d1ed368a19e43
alex9249 <alex9249@hotmail.com>
2014-12-13 16:21:25 -0500

    Sounds Implemented

--- app/src/main/java/com/example/alex/androidproject/Horse.java
+++ app/src/main/java/com/example/alex/androidproject/Horse.java
@@ -1,19 +1,52 @@
 package com.example.alex.androidproject;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Alex on 12/11/2014.
  */
 public class Horse extends Activity {
+    private SoundPool soundPool;
+    private int soundID;
+    boolean plays = false, loaded = false;
+    float actVolume, maxVolume, volume;
+    AudioManager audioManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.horse);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        actVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = actVolume / maxVolume;
+
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+        soundID = soundPool.load(this, R.raw.horse, 1);
+
       getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public void playSound(View v) {
+        // Is the sound loaded does it already play?
+        if (loaded && !plays) {
+            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+
+        }
     }
 }
 
